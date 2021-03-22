6663e448035af6c12de4cc4c18cf4b5f37a6a5a8
marceta99 <75267676+marceta99@users.noreply.github.com>
2021-03-22 22:36:40 +0100

    Add files via upload

---
+++ IgricaBrojeviSolution/IgricaBrojevi/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IgricaBrojevi
+{
+    public partial class Form1 : Form
+    {
+
+        private DateTime pocetnoVreme;
+        private Random ran; 
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int brojacKlikova = int.Parse(button1.Text);
+
+            brojacKlikova++;
+
+            
+
+            button1.Text = brojacKlikova.ToString();
+
+            if (brojacKlikova == 10)
+            {
+                DateTime krajnjeVreme = DateTime.Now;
+                int sekunde = (krajnjeVreme - pocetnoVreme).Seconds;
+                int milisekunde = ((krajnjeVreme - pocetnoVreme).Milliseconds) % 1000; 
+                label1.Text = "Vreme : " + sekunde + ":" + milisekunde;
+
+                
+            }
+            
+            ran = new Random();
+
+            int xKordinata = ran.Next(0, this.Width - 100);
+            int yKordinata = ran.Next(0, this.Height - 100);
+
+            button1.Location = new Point(xKordinata, yKordinata);
+
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "0";
+            label1.Text = "";
+            pocetnoVreme = DateTime.Now;
+            ran = new Random();
+
+            int xKordinata = ran.Next(0, this.Width - 100);
+            int yKordinata = ran.Next(0, this.Height - 100);
+
+            button1.Location = new Point(xKordinata, yKordinata); 
+        }
+    }
+}
